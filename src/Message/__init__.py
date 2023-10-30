c5cf62b6be752d46fd63a6c561311fb6a9260657
1mk0v <potapchuk01@mail.ru>
2023-10-30 18:10:14 +0300

    Add message template

---
+++ src/Message/__init__.py
@@ -0,0 +1,9 @@
+class StaticMessages:
+
+    @staticmethod
+    def helloMsg(name):
+        return f"Здравствуй, {name}!\n"
+    
+    @staticmethod
+    def getGroupMsg():
+        return F"Напиши пожалуйста номер своей группы!"
